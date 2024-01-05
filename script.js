c2bba599589ecab30360f0493addea93610c9e93
Oswaldousb31 <80560471+Oswaldousb31@users.noreply.github.com>
2024-01-05 15:21:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 var noButton = document.getElementById("noButton");
 var buttonWidth = noButton.offsetWidth;
 var buttonHeight = noButton.offsetHeight;
-var isMoving = false;
 
 noButton.addEventListener("mouseenter", function() {
   moveButton();
 });
 
 function moveButton() {
-  if (!isMoving) {
-    var windowWidth = window.innerWidth - buttonWidth;
-    var windowHeight = window.innerHeight - buttonHeight;
+  var windowWidth = window.innerWidth - buttonWidth;
+  var windowHeight = window.innerHeight - buttonHeight;
 
-    var xPos = Math.floor(Math.random() * windowWidth);
-    var yPos = Math.floor(Math.random() * windowHeight);
+  var xPos = Math.floor(Math.random() * windowWidth);
+  var yPos = Math.floor(Math.random() * windowHeight);
 
-    noButton.style.top = yPos + "px";
-    noButton.style.left = xPos + "px";
-
-    isMoving = true;
-
-    setTimeout(function() {
-      isMoving = false;
-    }, 1000);
-  }
+  noButton.style.top = yPos + "px";
+  noButton.style.left = xPos + "px";
 }
 
 document.addEventListener("mousemove", function(event) {
@@ -40,8 +31,3 @@ document.addEventListener("mousemove", function(event) {
     moveButton();
   }
 });
-
-moveButton();
-});
-
-moveButton();
