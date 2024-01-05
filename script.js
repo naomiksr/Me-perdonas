c268d5727c8be02c32315f743150b5d6931212c8
Oswaldousb31 <80560471+Oswaldousb31@users.noreply.github.com>
2024-01-05 15:04:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-// Esperar a que se cargue el contenido de la página
-document.addEventListener("DOMContentLoaded", function(event) {
-    var noButton = document.getElementById("noButton");
+var button = document.getElementById("button");
+var buttonWidth = button.offsetWidth;
+var buttonHeight = button.offsetHeight;
+var windowWidth = window.innerWidth - buttonWidth;
+var windowHeight = window.innerHeight - buttonHeight;
 
-    noButton.addEventListener("mouseenter", function(event) {
-        // Randomly generate new position
-        var xPos = Math.floor(Math.random() * (window.innerWidth - noButton.offsetWidth));
-        var yPos = Math.floor(Math.random() * (window.innerHeight - noButton.offsetHeight));
+button.addEventListener("mouseenter", function(event) {
+  moveButton();
+});
 
-        // Set new position
-        noButton.style.top = yPos + "px";
-        noButton.style.left = xPos + "px";
-    });
+function moveButton() {
+  var xPos = Math.floor(Math.random() * windowWidth);
+  var yPos = Math.floor(Math.random() * windowHeight);
+
+  button.style.top = yPos + "px";
+  button.style.left = xPos + "px";
+}
+
+document.addEventListener("mousemove", function(event) {
+  var cursorX = event.clientX;
+  var cursorY = event.clientY;
+  var buttonRect = button.getBoundingClientRect();
+
+  if (
+    cursorX >= buttonRect.left &&
+    cursorX <= buttonRect.right &&
+    cursorY >= buttonRect.top &&
+    cursorY <= buttonRect.bottom
+  ) {
+    moveButton();
+  }
 });
