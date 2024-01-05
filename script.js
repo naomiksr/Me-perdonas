186db140aa4ae1acb2a3ed12f66d2dd6907b8560
Oswaldousb31 <80560471+Oswaldousb31@users.noreply.github.com>
2024-01-05 15:06:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ var buttonWidth = button.offsetWidth;
 var buttonHeight = button.offsetHeight;
 var windowWidth = window.innerWidth - buttonWidth;
 var windowHeight = window.innerHeight - buttonHeight;
+var isMoving = false;
 
 button.addEventListener("mouseenter", function(event) {
   moveButton();
 });
 
 function moveButton() {
-  var xPos = Math.floor(Math.random() * windowWidth);
-  var yPos = Math.floor(Math.random() * windowHeight);
+  if (!isMoving) {
+    var xPos = Math.floor(Math.random() * windowWidth);
+    var yPos = Math.floor(Math.random() * windowHeight);
 
-  button.style.top = yPos + "px";
-  button.style.left = xPos + "px";
+    button.style.top = yPos + "px";
+    button.style.left = xPos + "px";
+
+    isMoving = true;
+
+    setTimeout(function() {
+      isMoving = false;
+    }, 1000); // Detener el movimiento después de 1 segundo
+  }
 }
 
 document.addEventListener("mousemove", function(event) {
